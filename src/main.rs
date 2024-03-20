831ad5e922a5ec66e774d18f09399eb766a8d464
mark <mstradling@gmail.com>
2024-03-20 21:46:24 +0000

    first branch

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let number: i32 = 3;
-    let number = number * 50;
+    let number: i32 = number * 50;
     println!("{number}");
     println!("this is more text");
     println!("this is more of the same");
-    let list = [1, 2, 3, 4, 5, 6];
+    let list: [i32; 6] = [1, 2, 3, 4, 5, 6];
     for number in list{
         print!("{number}\t");
     }
